12092d4ffcba6c22159976d8a396df87586e5583
fredlc <fredlefevre@outlook.com>
2020-03-29 13:18:19 -0400

    Added note model, lockStatus enum and dummy data

---
+++ SecureNotes/Enums/LockStatus.swift
@@ -0,0 +1,14 @@
+//
+//  LockStatus.swift
+//  SecureNotes
+//
+//  Created by Fred Lefevre on 2020-03-29.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+
+enum LockStatus {
+    case locked
+    case unlocked
+}
---
+++ SecureNotes/Model/Note.swift
@@ -0,0 +1,19 @@
+//
+//  Note.swift
+//  SecureNotes
+//
+//  Created by Fred Lefevre on 2020-03-29.
+//  Copyright © 2020 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    public private(set) var message: String
+    public var lockStatus: LockStatus
+    
+    init(message: String, lockStatus: LockStatus) {
+        self.message = message
+        self.lockStatus = lockStatus
+    }
+}
